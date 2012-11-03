c085fae0b8fe993b4ea9a14cf5c6d89759ff60b0
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-03 15:51:57 -0700

    Sho' 'nuff seems to work.

--- UI-in-both-code-and-XML/src/com/github/offby1/ui_in_both_code_and_xml/MainActivity.java
+++ UI-in-both-code-and-XML/src/com/github/offby1/ui_in_both_code_and_xml/MainActivity.java
@@ -3,13 +3,21 @@ package com.github.offby1.ui_in_both_code_and_xml;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        TextView nameValue = (TextView)findViewById (R.id.nameValue);
+        nameValue.setText ("John Doe");
+
+        TextView addrValue = (TextView)findViewById (R.id.addrValue);
+        addrValue.setText ("911 Both Blvd");
     }
 
     @Override
