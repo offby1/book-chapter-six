2556836e960ffa4aeedba3069341081d52af7387
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-04 12:11:24 -0800

    Copy-n-paste the button declaration from one .xml file into the other.  Ka-blooie!

--- two-different-activities/src/com/github/offby1/twodifferentactivities/OtherActivity.java
+++ two-different-activities/src/com/github/offby1/twodifferentactivities/OtherActivity.java
@@ -2,12 +2,29 @@ package com.github.offby1.twodifferentactivities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class OtherActivity extends Activity {
+    Button b;
+    OtherActivity this_activity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.darker_layout);
+
+        b = (Button)this.findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    // this feels wrong, but ...
+                    Intent i = new Intent("com.github.offby1.intent.action.MAIN");
+                    this_activity.startActivity(i);
+                }
+            });
     }
 }
