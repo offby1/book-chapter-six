c84d0ea9ea3decce11a1acaabe8040b2b6d131b2
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-04 10:31:52 -0800

    It has a button!  It does something!

--- two-different-activities/src/com/github/offby1/twodifferentactivities/MainActivity.java
+++ two-different-activities/src/com/github/offby1/twodifferentactivities/MainActivity.java
@@ -1,15 +1,32 @@
 package com.github.offby1.twodifferentactivities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button b;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button)this.findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Intent.ACTION_VIEW,
+                                          Uri.parse("http://github.com"));
+                    startActivity(i);
+                }
+            });
     }
 
     @Override
