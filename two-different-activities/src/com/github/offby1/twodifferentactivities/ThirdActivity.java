26c6a73f7f31e586bbf93231283f40090af564df
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-04 16:21:33 -0800

    Give the third activity a "Settings" menu, just for consistency.

--- two-different-activities/src/com/github/offby1/twodifferentactivities/ThirdActivity.java
+++ two-different-activities/src/com/github/offby1/twodifferentactivities/ThirdActivity.java
@@ -3,7 +3,7 @@ package com.github.offby1.twodifferentactivities;
 import android.os.Bundle;
 import android.app.Activity;
 
-public class ThirdActivity extends Activity {
+public class ThirdActivity extends HasASettingsMenuActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
