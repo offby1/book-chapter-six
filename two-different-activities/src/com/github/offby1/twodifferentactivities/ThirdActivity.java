a4a986b079e2e470ec70e280ce600f76b365aab9
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-04 17:59:00 -0800

    Nix unused import

--- two-different-activities/src/com/github/offby1/twodifferentactivities/ThirdActivity.java
+++ two-different-activities/src/com/github/offby1/twodifferentactivities/ThirdActivity.java
@@ -1,7 +1,6 @@
 package com.github.offby1.twodifferentactivities;
 
 import android.os.Bundle;
-import android.app.Activity;
 
 public class ThirdActivity extends HasASettingsMenuActivity {
     @Override
